342005357f70b276a1ca9539f57f25cafcf76336
Rob Bianca <rjbianca@hotmail.com>
2018-07-03 03:42:25 -0400

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Rob Bianca on 7/2/18.
+//  Copyright © 2018 Robert Bianca. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    
+    var title : String = ""
+    
+    var done : Bool = false
+    
+}
